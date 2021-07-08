495d47548e1cae1c696f096665f69649a4e77a9b
lucastrs1 <77109386+lucastrs1@users.noreply.github.com>
2021-07-08 16:49:19 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function search_articles() {
+    const input = document.getElementById('input');
+    const filter = input.value.toUpperCase();
+    const ul = document.getElementById("selection");
+    const li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
